fb23106f493c4eb37a6cd2af46e69ee2ff492727
kalinin-osdh <kalinin.osdh@gmail.com>
2022-09-09 21:50:21 +0300

    Model is done

---
+++ src/main/java/Model/Admin.java
@@ -0,0 +1,39 @@
+package Model;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+
+@Entity
+@Table(name = "Admin")
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer Id;
+
+    @Column(name = "Name")
+    private String Name;
+
+    public Admin(){
+    }
+
+    public Admin(Integer Id, String Name){
+        this.Id = Id;
+        this.Name = Name;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
---
+++ src/main/java/Model/Comment.java
@@ -0,0 +1,80 @@
+package Model;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+
+import java.sql.Time;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "Comment")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer Id;
+
+    @ManyToMany(mappedBy = "Comments")
+    private Set<Post> Posts = new HashSet<>();
+
+    @Column(name = "Text")
+    private String Text;
+
+    @Column(name = "Time")
+    private Time Time;
+
+    @ManyToOne
+    @JoinColumn(name = "Id", nullable = false)
+    private User User;
+
+    public Comment() {
+    }
+
+    public Comment(Integer id, Set<Post> posts, String text, java.sql.Time time, Model.User user) {
+        Id = id;
+        Posts = posts;
+        Text = text;
+        Time = time;
+        User = user;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public Set<Post> getPosts() {
+        return Posts;
+    }
+
+    public void setPosts(Set<Post> posts) {
+        Posts = posts;
+    }
+
+    public String getText() {
+        return Text;
+    }
+
+    public void setText(String text) {
+        Text = text;
+    }
+
+    public java.sql.Time getTime() {
+        return Time;
+    }
+
+    public void setTime(java.sql.Time time) {
+        Time = time;
+    }
+
+    public Model.User getUser() {
+        return User;
+    }
+
+    public void setUser(Model.User user) {
+        User = user;
+    }
+}
